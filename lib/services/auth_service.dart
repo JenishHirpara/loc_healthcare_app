2478746b82fd216951a7b71ee59e6bc75d776773
JenishHirpara <jenishhirpara23@gmail.com>
2022-12-11 12:14:28 +0530

    first commit

--- lib/services/auth_service.dart
+++ lib/services/auth_service.dart
@@ -5,44 +5,47 @@ import 'package:healthcare/screens/login_page.dart';
 import 'package:healthcare/screens/register_page.dart';
 import 'package:healthcare/services/db_connections.dart';
 
-class AuthService{
+class AuthService {
   getCurrentUserPhoneNumber() async {
     FirebaseUser currentUser;
     currentUser = await FirebaseAuth.instance.currentUser();
     return currentUser.phoneNumber;
   }
+
   //Handling Auth
   handleAuth() {
     return StreamBuilder(
       stream: FirebaseAuth.instance.onAuthStateChanged,
-      builder: (BuildContext context, snapshot){
-        if(snapshot.hasData)
-        {
+      builder: (BuildContext context, snapshot) {
+        if (snapshot.hasData) {
           return FutureBuilder(
             future: DBConnections().verifyUser(snapshot.data.phoneNumber),
-            builder: (BuildContext context, AsyncSnapshot snapshot){
-              if(snapshot.data == true)
+            builder: (BuildContext context, AsyncSnapshot snapshot) {
+              if (snapshot.data == true)
                 return DashboardPage();
-              else{
+              else {
                 return RegisterPage();
               }
             },
           );
-        }
-        else{
+        } else {
           return LoginPage();
         }
       },
     );
   }
-  signOut(){
+
+  signOut() {
     FirebaseAuth.instance.signOut();
   }
-  signIn(AuthCredential authCredential){
+
+  signIn(AuthCredential authCredential) {
     FirebaseAuth.instance.signInWithCredential(authCredential);
   }
-  signInWithOTP(smsCode, verId){
-    AuthCredential authCredential = PhoneAuthProvider.getCredential(verificationId: verId, smsCode: smsCode);
+
+  signInWithOTP(smsCode, verId) {
+    AuthCredential authCredential = PhoneAuthProvider.getCredential(
+        verificationId: verId, smsCode: smsCode);
     signIn(authCredential);
   }
 }
